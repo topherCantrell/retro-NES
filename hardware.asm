f83331e9b18bdae9d98290f551ce39119ea66a7b
Christopher Cantrell <topherCantrell@gmail.com>
2024-08-29 15:07:55 -0500

    Update hardware.asm

--- hardware.asm
+++ hardware.asm
@@ -4,9 +4,8 @@
 ; 0800 - 1FFF   (unused -- mirrors)
 ; 2000 - 4FFF   Memory Mapped  Registers
 ; 5000 - 5FFF   Expansion modules (keyboards, etc.)
-; 6000 - 7FFF   8K Cartridge RAM (WRAM
-; 8000 - BFFF   16K Cartridge ROM Lower Bank
-; C000 - FFFF   16K Cartridge ROM Upper Bank
+; 6000 - 7FFF   8K Cartridge RAM (WRAM)
+; 8000 - FFFF   32K Cartridge ROM
 
 ; 6502 hardware vectors
 ;

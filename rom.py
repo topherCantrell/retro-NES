4b5fa766cd9df8175f192262f3c2a6750edf2910
Chris Cantrell <tophercantrell@gmail.com>
2024-09-20 20:16:13 -0500

    zelda bin for testing

--- rom.py
+++ rom.py
@@ -1,8 +1,8 @@
 
-with open('KidIcarus.nes','rb') as f:
+with open('Zelda.nes','rb') as f:
     rom = f.read()
 
-    with open('kidicarus.bin', 'wb') as g:
+    with open('zelda.bin', 'wb') as g:
         g.write(rom[16:])
 
     
